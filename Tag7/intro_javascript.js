9a3f66c543f575292e586a3b72bab1e8d8d7990a
DesiAndina <desiree.andina@stud.sfgz.ch>
2024-10-04 17:51:59 +0200

    Finale Daten

---
+++ Tag7/intro_javascript.js
@@ -0,0 +1,66 @@
+/*console.log(document.getElementById("element")
+);*/
+
+let n = 0;
+console. log(n);
+
+n=n+1;
+console.log(n);
+
+function buttonPressed() {
+    n=n+100;
+
+    document.getElementById("element").style.left = n + "px";
+
+}
+
+function neuesElement() {
+    let div = document.createElement("div");
+    div.classList.add('random');
+    document.body.appendChild(div);
+
+
+}
+
+function starteAnimation() {
+    let element = document.getElementById('kreis2');
+    element.style.animationPlayState = "running";
+ 
+}
+
+
+window.addEventListener("scroll", starteAnimation);
+
+
+var xp = 0,
+  mouseX = 0;
+var yp = 0,
+  mouseY = 0;
+var xpDot = 0,
+  mouseX = 0;
+var ypDot = 0,
+  mouseY = 0;
+
+const cursorFollower = document.querySelector(".cursorFollower");
+const cursorFollowerDot = document.querySelector(".cursorFollowerDot");
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+});
+
+setInterval(function () {
+  xp += (mouseX - xp) / 15;
+  yp += (mouseY - yp) / 15;
+
+  cursorFollower.style.left = xp + "px";
+  cursorFollower.style.top = yp + "px";
+}, 20);
+
+setInterval(function () {
+  xpDot += (mouseX - xpDot) / 25;
+  ypDot += (mouseY - ypDot) / 25;
+
+  cursorFollowerDot.style.left = xpDot + "px";
+  cursorFollowerDot.style.top = ypDot + "px";
+}, 20);
